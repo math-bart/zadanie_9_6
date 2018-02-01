9c0c7c0cf7a6d179673eeb1dbb329a1bef8f138c
Bartosz Wojtaś <el.klides@op.pl>
2018-02-01 17:24:20 +0100

    Add index.html and scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var buttonElem = document.getElementById('addElem');
+var listElem = document.getElementById('list');
+
+buttonElem.addEventListener('click', function (e) {
+    var itemsByTagName = document.getElementsByTagName('li');
+    var counter = itemsByTagName.length;
+	var newElem = document.createElement('li');
+    newElem.innerHTML = 'item ' + counter;
+    listElem.appendChild(newElem);
+});
