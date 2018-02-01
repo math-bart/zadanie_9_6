639068e0782e15122484aebae5738c48eff8fb8f
Bartosz Wojtaś <el.klides@op.pl>
2018-02-01 17:27:31 +0100

    Change scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var listElem = document.getElementById('list');
 buttonElem.addEventListener('click', function (e) {
     var itemsByTagName = document.getElementsByTagName('li');
     var counter = itemsByTagName.length;
-	var newElem = document.createElement('li');
+    var newElem = document.createElement('li');
     newElem.innerHTML = 'item ' + counter;
     listElem.appendChild(newElem);
 });
